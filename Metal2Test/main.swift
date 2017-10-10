fdb727fa9d40143ab405a2a8256a6dbff86f9048
Casey Duckering <cduckering@gmail.com>
2017-10-09 19:32:02 -0700

    Initial Commit

---
+++ Metal2Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Metal2Test
+//
+//  Created by Casey Duckering on 2017-10-09.
+//  Copyright © 2017 Casey Duckering. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
